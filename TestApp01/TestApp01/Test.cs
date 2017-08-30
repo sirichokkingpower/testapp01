e7a7d74e2b1b80f8cc41a97081412e49a73f5e02
Sirichok <zapz1st@gmail.com>
2017-08-30 22:53:44 +0700

    Edit Test 02

--- TestApp01/TestApp01/Test.cs
+++ TestApp01/TestApp01/Test.cs
@@ -8,5 +8,7 @@ namespace TestApp01
     public class Test
     {
         private string name = "H1o2n3da";
+        private string name2 = "H1o2n3da5555555555";
+        private string name3 = "H1o2n3da4444444444";
     }
 }
