dc248fdf4719e519d09a3777ae80a21982c23004
Sirichok <zapz1st@gmail.com>
2017-08-30 22:44:40 +0700

    Add Test class.

---
+++ TestApp01/TestApp01/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp01
+{
+    public class Test
+    {
+        private string name = "Honda";
+    }
+}
